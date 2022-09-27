c63f506a5fe4f59712b85826085931dc4c421b5c
SCHMALBACH\andre <andres.f.21.as@gmail.com>
2022-09-27 16:26:22 -0500

    no message

---
+++ src/app/tab3/mercadopago.php
@@ -0,0 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+MercadoPago\SDK::setAccessToken('TEST-4907284874211732-080720-c348f2643758824729fad8e09c5f81b7-232160373');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>

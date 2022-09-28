06fbd9585c241bc5ed5de334ca7e0c9468693e89
josephv_2210 <joseph.2210@hotmail.es>
2022-09-27 19:11:15 -0500

    create and fixes

--- src/app/pages/create-proyect/create-proyect.module.ts
+++ src/app/pages/create-proyect/create-proyect.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CreateProyectPageRoutingModule } from './create-proyect-routing.module';
 
 import { CreateProyectPage } from './create-proyect.page';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CreateProyectPageRoutingModule
+    CreateProyectPageRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [CreateProyectPage]
 })
